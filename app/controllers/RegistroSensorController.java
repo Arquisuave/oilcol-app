475c9628fcbf028921c32f80ab91d142ce7e9d3c
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2016-08-28 18:52:53 -0500

    Controlles sensores

--- app/controllers/RegistroSensorController.java
+++ app/controllers/RegistroSensorController.java
@@ -4,6 +4,9 @@ import akka.dispatch.MessageDispatcher;
 import com.fasterxml.jackson.databind.JsonNode;
 import dispatchers.AkkaDispatcher;
 import models.RegistroSensorBarrilesEntity;
+import models.RegistroSensorEmergEntity;
+import models.RegistroSensorEnerEntity;
+import models.RegistroSensorTempEntity;
 import play.libs.*;
 import play.mvc.*;
 
@@ -58,6 +61,148 @@ public class RegistroSensorController {
         ).thenApply(
                 registroSensorBarrilesEntity -> {
                     return ok(Json.toJson(registroSensorBarrilesEntity));
+                }
+        );
+    }
+
+    //CRUD sensor emergencia
+    public CompletionStage<Result> getRegistrosSensorEmerg() {
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+
+        return CompletableFuture.
+                supplyAsync(
+                        () -> {
+                            return RegistroSensorEmergEntity.FINDER.all();
+                        }
+                        ,jdbcDispatcher)
+                .thenApply(
+                        registroSensorEmergEntities -> {
+                            return ok(toJson(registroSensorEmergEntities));
+                        }
+                );
+    }
+
+    public CompletionStage<Result> createRegistroSensorEmerg(){
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+        JsonNode nRegistroSensorEmerg = request().body().asJson();
+        RegistroSensorEmergEntity reg = Json.fromJson( nRegistroSensorEmerg , RegistroSensorEmergEntity.class ) ;
+        return CompletableFuture.supplyAsync(
+                ()->{
+                    reg.save();
+                    return reg;
+                }
+        ).thenApply(
+                registroSensorEmergEntity -> {
+                    return ok(Json.toJson(registroSensorEmergEntity));
+                }
+        );
+    }
+
+    public CompletionStage<Result> getRegistroSensorEmerg(Long id){
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+        return CompletableFuture.supplyAsync(
+                ()->{
+                    return RegistroSensorEmergEntity.FINDER.byId(id);
+                }
+                ,jdbcDispatcher
+        ).thenApply(
+                registroSensorEmergEntity -> {
+                    return ok(Json.toJson(registroSensorEmergEntity));
+                }
+        );
+    }
+
+    //CRUD sensor energía
+    public CompletionStage<Result> getRegistrosSensorEner() {
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+
+        return CompletableFuture.
+                supplyAsync(
+                        () -> {
+                            return RegistroSensorEnerEntity.FINDER.all();
+                        }
+                        ,jdbcDispatcher)
+                .thenApply(
+                        registroSensorEnerEntities -> {
+                            return ok(toJson(registroSensorEnerEntities));
+                        }
+                );
+    }
+
+    public CompletionStage<Result> createRegistroSensorEner(){
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+        JsonNode nRegistroSensorEner = request().body().asJson();
+        RegistroSensorEnerEntity reg = Json.fromJson( nRegistroSensorEner , RegistroSensorEnerEntity.class ) ;
+        return CompletableFuture.supplyAsync(
+                ()->{
+                    reg.save();
+                    return reg;
+                }
+        ).thenApply(
+                registroSensorEnerEntity -> {
+                    return ok(Json.toJson(registroSensorEnerEntity));
+                }
+        );
+    }
+
+    public CompletionStage<Result> getRegistroSensorEner(Long id){
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+        return CompletableFuture.supplyAsync(
+                ()->{
+                    return RegistroSensorEnerEntity.FINDER.byId(id);
+                }
+                ,jdbcDispatcher
+        ).thenApply(
+                registroSensorEnerEntity -> {
+                    return ok(Json.toJson(registroSensorEnerEntity));
+                }
+        );
+    }
+
+    // CRUD sensor temperatura
+
+    public CompletionStage<Result> getRegistrosSensorTemp() {
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+
+        return CompletableFuture.
+                supplyAsync(
+                        () -> {
+                            return RegistroSensorTempEntity.FINDER.all();
+                        }
+                        ,jdbcDispatcher)
+                .thenApply(
+                        registroSensorTempEntities -> {
+                            return ok(toJson(registroSensorTempEntities));
+                        }
+                );
+    }
+
+    public CompletionStage<Result> createRegistroSensorTemp(){
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+        JsonNode nRegistroSensorTemp = request().body().asJson();
+        RegistroSensorTempEntity reg = Json.fromJson( nRegistroSensorTemp , RegistroSensorTempEntity.class ) ;
+        return CompletableFuture.supplyAsync(
+                ()->{
+                    reg.save();
+                    return reg;
+                }
+        ).thenApply(
+                registroSensorTempEntity -> {
+                    return ok(Json.toJson(registroSensorTempEntity));
+                }
+        );
+    }
+
+    public CompletionStage<Result> getRegistroSensorTemp(Long id){
+        MessageDispatcher jdbcDispatcher = AkkaDispatcher.jdbcDispatcher;
+        return CompletableFuture.supplyAsync(
+                ()->{
+                    return RegistroSensorTempEntity.FINDER.byId(id);
+                }
+                ,jdbcDispatcher
+        ).thenApply(
+                registroSensorTempEntity -> {
+                    return ok(Json.toJson(registroSensorTempEntity));
                 }
         );
     }
