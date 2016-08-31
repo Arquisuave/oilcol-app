9a02952b7da17ecec27ce815bb092e922b2139ec
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2016-08-30 20:11:11 -0500

    Sensor energía

--- app/logic/RegistroSensorEnerLogic.java
+++ app/logic/RegistroSensorEnerLogic.java
@@ -1,7 +1,34 @@
 package logic;
 
+import models.RegistroSensorEnerEntity;
+
 /**
- * Created by Usuario on 28/08/2016.
+ * Created by mmgomez on 28/08/2016.
  */
 public class RegistroSensorEnerLogic {
+
+
+    public static RegistroSensorEnerEntity promediar (RegistroSensorEnerEntity reg){
+        boolean necesita = false;
+        RegistroSensorEnerEntity regActual = RegistroSensorEnerEntity.FINDER
+                .where().eq("pozo",reg.getPozo()).orderBy("timestamp desc").setFirstRow(1).findUnique();
+        if (regActual != null && reg.getTimeStamp().getHours() == regActual.getTimeStamp().getHours()){
+            double promedioActual = regActual.getInfo();
+            int numEntradas = regActual.getNumEntradas();
+            double infoNueva = reg.getInfo();
+            double promedioPonderado = ((promedioActual * numEntradas) + infoNueva)/(numEntradas+1);
+            regActual.setNumEntradas(numEntradas+1);
+            regActual.setInfo(promedioPonderado);
+            regActual.update();
+            return regActual;
+        }
+        else{
+            reg.setNumEntradas(1);
+            reg.save();
+            return reg;
+        }
+
+    }
+
+
 }
