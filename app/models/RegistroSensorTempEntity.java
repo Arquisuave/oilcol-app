d938b707b79612959862d9fbe2d53d550abd4a1b
cgarciahdez <c.garcia11@uniandes.edu.co>
2016-09-18 16:52:12 -0500

    Arreglitos

--- app/models/RegistroSensorTempEntity.java
+++ app/models/RegistroSensorTempEntity.java
@@ -64,7 +64,7 @@ public class RegistroSensorTempEntity extends Model
     @NotNull
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "TIMESTAMP")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'hh:mm:ssX")
     private Date timeStamp;
 
     public Date getTimeStamp()
