9e39fdd9efa824b87e510b3394fe7e9b2bf967dd
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2016-09-18 13:02:18 -0500

    Pushed last revision to upstream

--- app/logic/RegistroSensorBarrilesLogic.java
+++ app/logic/RegistroSensorBarrilesLogic.java
@@ -11,26 +11,26 @@ public class RegistroSensorBarrilesLogic
 
     public static RegistroSensorBarrilesEntity crearActualizarRegistro(RegistroSensorBarrilesEntity entityBarril)
     {
-        RegistroSensorBarrilesEntity ultimoRegistro = RegistroSensorBarrilesEntity.FINDER.where().eq("pozo", entityBarril.getPozo()).orderBy("id_sensor_barriles desc").setMaxRows(1).findUnique();
+        //RegistroSensorBarrilesEntity ultimoRegistro = RegistroSensorBarrilesEntity.FINDER.where().eq("pozo", entityBarril.getPozo()).orderBy("id_sensor_barriles desc").setMaxRows(1).findUnique();//
 
-        // si entra actualiza
-        if (ultimoRegistro !=null && (ultimoRegistro.getTimeStamp().getHours() == entityBarril.getTimeStamp().getHours()))
-        {
-            double promedio =  ultimoRegistro.getInfo()* ultimoRegistro.getNumEntradas()+ ultimoRegistro.getInfo();
+//        // si entra actualiza
+//        if (ultimoRegistro !=null && (ultimoRegistro.getTimeStamp().getHours() == entityBarril.getTimeStamp().getHours()))
+//        {
+//            double promedio =  ultimoRegistro.getInfo()* ultimoRegistro.getNumEntradas()+ ultimoRegistro.getInfo();//
 
-            promedio= promedio/(ultimoRegistro.getNumEntradas()+1);
+//            promedio= promedio/(ultimoRegistro.getNumEntradas()+1);//
 
-            ultimoRegistro.setNumEntradas(ultimoRegistro.getNumEntradas()+1);
-            ultimoRegistro.setInfo(promedio);
-            ultimoRegistro.update();
-            return ultimoRegistro;
-        }
-        else
-        {
+//            ultimoRegistro.setNumEntradas(ultimoRegistro.getNumEntradas()+1);
+//            ultimoRegistro.setInfo(promedio);
+//            ultimoRegistro.update();
+//            return ultimoRegistro;
+//        }
+//        else
+//        {
             //crea el nuevo registro
             entityBarril.setNumEntradas(1);
             entityBarril.save();
             return entityBarril;
-        }
+        //}
     }
 }
