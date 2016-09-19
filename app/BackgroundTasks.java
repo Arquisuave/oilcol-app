b9d71608c2facacd691b17f2f3cc582008110ec4
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2016-09-18 20:33:53 -0500

    background task complete!

--- app/BackgroundTasks.java
+++ app/BackgroundTasks.java
@@ -1,3 +1,5 @@
+import controllers.PozoController;
+import models.PozoEntity;
 import models.RegistroSensorTempEntity;
 import play.Logger;
 import play.api.inject.ApplicationLifecycle;
@@ -5,6 +7,8 @@ import play.libs.F;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import java.util.List;
 
 import static java.lang.Thread.sleep;
 
@@ -17,14 +21,44 @@ public class BackgroundTasks {
     @Inject
     public BackgroundTasks(ApplicationLifecycle lifecycle)
     {
+        System.out.println(" llega hasta antes de background");
         Runnable r = new Runnable() {
             public void run()
             {
                 while(true)
                 {
-                    RegistroSensorTempEntity.FINDER.
+                    double promedio = 0;
+
+
+                    for(long j=1;j<= 1000;j++)// revisa por cada pozo
+                    {
+                        PozoEntity pozo = PozoEntity.FINDER.byId(j);
+                        RegistroSensorTempEntity nuevo = new RegistroSensorTempEntity( pozo, null,0);
+
+                        List<RegistroSensorTempEntity> tempEntities = RegistroSensorTempEntity.FINDER.where().eq("ENTRADAS",  1).eq("pozo", pozo).orderBy("id_sensor_temp desc").setMaxRows(3600).findList();
+                        for(int i=0;i< tempEntities.size(); i++)
+                        {
+                            RegistroSensorTempEntity actual = tempEntities.get(i);
+
+                            promedio= nuevo.getInfo()*nuevo.getNumEntradas()+ actual.getInfo();
+                            promedio/=(nuevo.getNumEntradas()+1);
+
+                            nuevo.setInfo(promedio);
+                            nuevo.setNumEntradas(nuevo.getNumEntradas()+1);
+
+                            if(i==tempEntities.size()-1){nuevo.setTimeStamp(actual.getTimeStamp());}
+                        }
+                        if(nuevo.getInfo() != 0)
+                        {
+                            nuevo.save();
+                        }
+
+                    }
+
+
                     try {
-                        sleep(1000);
+                        sleep(10000000);//100 segundos
+                        System.out.println("Se despierta a hacer cosas");
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -33,6 +67,6 @@ public class BackgroundTasks {
         };
         Thread t = new Thread(r);
         t.start();
-        System.out.println(" Ya esta corriendo en background ");
+        System.out.println(" Ya esta corriendo en background el promediador");
     }
 }
