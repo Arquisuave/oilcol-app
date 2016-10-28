c9b51d9cbe273b889b0358b99a89fee399b2ce44
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2016-10-27 21:19:54 -0500

    Commitcito

---
+++ public/js/campos.js
@@ -0,0 +1,76 @@
+/**
+ * Created by margarita on 27/10/2016.
+ */
+/**
+ * Aquí hago las cositas de los pozos
+ */
+
+//Get pozos
+$.ajax({
+    method: "GET",
+    url:"/pozo"
+}).done(function(msg){
+    console.log(msg [5]);
+    var pozos = [];
+    for(var i=0;i<msg.length;i++){
+        var este = {idPozo:msg[i].id,long:msg[i].lon,latt:msg[i].lat,est:msg[i].estado};
+        var pozoActual = [este.idPozo,este.long,este.latt,este.est];
+        pozos.push(pozoActual);
+    }
+    console.log(pozos);
+    $('#tablita').DataTable({
+        data: pozos,
+        columns: [
+            { title: "ID Pozo"},
+            { title: "Longitud"},
+            { title: "Latitud"},
+            { title: "Estado"}
+        ]
+    });
+
+});
+
+//Post pozo
+$('#btnSave').click(function() {
+    console.log("Click en Guardar")
+    console.log($('#longitud').val());
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST", "/pozo");
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(JSON.stringify({
+        "lon": $('#longitud').val(),
+        "lat": $('#latitud').val(),
+        "estado": $('#estado').val(),
+        "campo": {
+            "id": 1,
+            "idJefeCampo": {
+                "username": "margarita",
+                "type": null,
+                "password": null,
+                "notifications": []
+            },
+            "region": "ANDINA",
+            "pozos": []
+        }
+    }));
+});
+
+//DELETE pozo
+$('#btnDelete').click(function() {
+    console.log("Click en Delete")
+    var idPozo = $('#idPozo').val();
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("DELETE", "/pozo/"+idPozo);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send();
+});
+
+//PUT pozo
+$('#btnNuevoEstado').click(function() {
+    console.log("Click en Edit")
+    var idPozo = $('#idPozo2').val();
+    var nuevoEstado = $('#nuevoEstado').val();
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("PUT", "/updatePozo/"+idPozo+"/"+nuevoEstado);
+    xmlhttp.send();
+});
