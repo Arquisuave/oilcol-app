d3c11e34a27310ed23212dd6944d34da66c0e4a4
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2016-11-29 17:56:39 -0500

    campitos deberia funcionar

--- misc/campitosJefecitos.py
+++ misc/campitosJefecitos.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+import random
 import numpy as np
 
 r=[]
@@ -10,7 +11,7 @@ headers = {'Oilcol-Token':'asdkljaskldjsakldjsad'}
 (requests.post('http://localhost:9000/users/register', headers=headers, json=user))
 
 
-user = {'username':'example@uniandes.edu.co', 'password':"lomaximo", 'type':'JEFE_CAMPO'}
+user = {'username':'jg.tamura@uniandes.edu.co', 'password':"lomaximo", 'type':'JEFE_CAMPO'}
 headers = {'Oilcol-Token':'asdkljaskldjsakldjsad'}
 requests.post('http://localhost:9000/users/register', headers=headers, json=user)
 
@@ -52,15 +53,18 @@ lon=[[-77.5,-76.5],[-76.6,-73.2],[-74.98,-73.6],[-70.9,-68.12],[-73.70,-70.16]]
 lats = [[2.33,5.8],[8.464,12.7],[5.15,6.3],[4.25,5.9],[-1.46,0.885]]
 
 print r
+estado = {1:'PARADO',2:'ABIERTO',4:'CLAUSURADO',3:'PRODUCCION'}
 
 for i in range(0, len(r)):
     print(i)
     print(r[i].json())
     _id = r[i].json()['id']
     for j in range(0, 50):
+        numeroT = np.around(np.random.triangular(1,4,2))
+        estadoT = estado[numeroT]
         lats = np.random.uniform(lats[i][0],lats[i][1])
         lon = np.random.uniform(lons[i][0],lons[i][1])
-        p = {'lon':lon,'lat':lat,'estado':'ABIERTO','campo':{'id':_id}}
+        p = {'lon':lon,'lat':lat,'estado': estadoT ,'campo':{'id':_id}}
         requests.post('http://localhost:9000/pozo', headers=headers, json=p)
 
 
